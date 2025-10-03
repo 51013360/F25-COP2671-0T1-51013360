9d820fb72dea90c3105b88a20ab935cff2aa03fb
51013360 <51013360@sfcollege.edu>
2025-10-03 12:50:09 -0400

    Lesson 5.2 - Keeping Score

--- Units/User Interface/Prototype 5/Assets/Scripts/Target.cs
+++ Units/User Interface/Prototype 5/Assets/Scripts/Target.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
+    public ParticleSystem explosionParticle;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 1;
     private float xRange = 4;
     private float ySpawnPos = -2;
+
+    public int pointValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +22,8 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -29,6 +35,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
